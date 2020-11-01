d86a8b3d6144f40db6b5a9bf11a102f3015a5ba5
randa1417 <rmalojairy@sm.imamu.edu.sa>
2020-11-01 23:20:44 +0300

    Add files via upload

---
+++ Transfer_learning_Covid/Covid-19_ResNet101.py
@@ -0,0 +1,82 @@
+from tensorflow.keras.applications.resnet import ResNet50 ,ResNet101, preprocess_input , decode_predictions
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D,MaxPooling2D,Flatten,Dense,Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import os
+import tensorflow as tf
+
+from tensorflow.keras import Model
+
+
+
+ResNet_101 = ResNet101(input_shape=[256,256,3] , weights='imagenet' , include_top=False)
+
+for layer in ResNet_101.layers:
+    layer.trainable = False
+
+x = Flatten()(ResNet_101.output)
+pred = Dense(2, activation='sigmoid')(x)
+model = Model(inputs=ResNet_101.input, outputs=pred)
+
+# model.summary()
+optimizer = tf.keras.optimizers.Adam(lr=.0001, clipnorm=0.0001)
+
+########################## Image DataGenerator ##########################
+model.compile(optimizer=optimizer, loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+train_gen = ImageDataGenerator(rescale=1. / 255,
+                               zoom_range=0.2,
+                               width_shift_range=0.1,
+                               height_shift_range=0.1,
+                               validation_split=0.2)
+
+# val_gen = ImageDataGenerator(rescale=1./255)
+
+trin_set = train_gen.flow_from_directory('Data_covid-19/train', target_size=(256, 256), batch_size=16,
+                                         subset='training')
+val_set = train_gen.flow_from_directory('Data_covid-19/train', target_size=(256, 256), batch_size=16,
+                                        subset='validation')
+
+########################## Fit ##########################
+history = model.fit(trin_set,
+                    steps_per_epoch=30,  # how num of image will be train
+                    epochs=80,  # num of cycle for per image & to make more accuracy (high epochs)--> epochs=15 or more
+                    # validation_split= 0.4 , # remove 20% from data & maker faster & less over fit
+                    validation_steps=10,
+                    validation_data=val_set)
+
+# print(trin_set.class_indices)
+
+train_acc = model.evaluate(trin_set, steps=4)
+val_acc = model.evaluate(val_set, steps=4)
+
+print('\n\n####### The Accuracy & Loos percentile #######')
+print('\n - For Training data set:')
+print("%s: %.2f%% " % (model.metrics_names[1], train_acc[1] * 100))
+print("Loss:", train_acc[0])
+
+print('\n - For validation data set:')
+print("%s: %.2f%% " % (model.metrics_names[1], val_acc[1] * 100))
+print("Loss:", val_acc[0])
+
+
+
+#model.save('Covid-19ResNet101.h5')
+
+'''
+ - For Training data set:
+    accuracy: 98.44% 
+    Loss: 0.09483683109283447
+
+ - For validation data set:
+    accuracy: 90.62% 
+    Loss: 0.2372460514307022
+'''
+
---
+++ Transfer_learning_Covid/Test_ResNet50.py
@@ -0,0 +1,31 @@
+from tensorflow.keras.applications.resnet import ResNet50 ,ResNet101, preprocess_input , decode_predictions
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D,MaxPooling2D,Flatten,Dense,Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt
+from glob import glob
+import numpy as np
+import random
+import cv2
+
+
+###################### load_model ######################
+new_model = load_model('Covid-19ResNet50.h5')
+
+
+
+###################### Predictions ######################
+img_path = glob('Data_covid-19/test_covid19/normal/*.*')
+
+for path in img_path:
+    im = cv2.imread(path)
+    im = cv2.resize(im, (256,256))/255
+    im = im.reshape(1,256,256,3)
+    # print(im.shape)
+    # [.8 , .2] ***
+    predic = new_model.predict(im)
+    #print(predic)
+    print(predic.argmax())
